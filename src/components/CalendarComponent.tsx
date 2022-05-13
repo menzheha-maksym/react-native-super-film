9663fbd47933bf671ae4843d981ac2b61a0987d5
Maksym Menzheha <masimfyd@gmail.com>
2022-05-13 23:44:48 +0300

    calendar header styles

--- src/components/CalendarComponent.tsx
+++ src/components/CalendarComponent.tsx
@@ -1,10 +1,20 @@
 import {ParamListBase} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import moment from 'moment';
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {Calendar} from 'react-native-calendars';
 import {useAppDispatch} from '../redux/hooks';
 import {setDateString} from '../redux/reducers/calendarSlice';
+
+const styles = StyleSheet.create({
+  headerStyle: {
+    backgroundColor: 'rgb(220,220,220)',
+  },
+  headerMonth: {
+    fontWeight: 'bold',
+  },
+});
 
 interface CalendarProps {
   navigation: NativeStackNavigationProp<ParamListBase>;
@@ -16,6 +26,11 @@ const CalendarComponent: React.FC<CalendarProps> = ({navigation}) => {
   return (
     <View>
       <Calendar
+        headerStyle={styles.headerStyle}
+        renderHeader={(date: Date) => (
+          <Text style={styles.headerMonth}>{moment(date).format('MMMM')}</Text>
+        )}
+        arrow
         onDayPress={day => {
           dispatch(setDateString(day));
           navigation.navigate('Episodes');
