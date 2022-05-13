982dd78b66be6c3d648ddf70e8d634881a64462b
Maksym Menzheha <masimfyd@gmail.com>
2022-05-13 14:14:52 +0300

    add episodes page

---
+++ src/pages/Episodes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Episodes: React.FC = ({}) => {
+  return (
+    <View>
+      <Text>Episodes</Text>
+    </View>
+  );
+};
+
+export default Episodes;
