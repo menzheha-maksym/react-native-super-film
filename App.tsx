17d502851ca99051b6b249568cc6b30bc40969d6
Maksym Menzheha <masimfyd@gmail.com>
2022-05-13 16:38:12 +0300

    Header Styles

--- App.tsx
+++ App.tsx
@@ -9,19 +9,39 @@
  */
 
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationOptions,
+} from '@react-navigation/native-stack';
 import React from 'react';
 import Episodes from './src/pages/Episodes';
 import Home from './src/pages/Home';
 
 const Stack = createNativeStackNavigator();
 
+const headerStyle: NativeStackNavigationOptions = {
+  headerStyle: {backgroundColor: 'rgb(220, 220, 220)'},
+  headerTintColor: '#180101',
+  headerTitleStyle: {
+    fontWeight: '100',
+  },
+  headerTitleAlign: 'center',
+};
+
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Episodes" component={Episodes} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{title: 'SUPER FILM', ...headerStyle}}
+        />
+        <Stack.Screen
+          name="Episodes"
+          component={Episodes}
+          options={{title: 'SUPER FILM', ...headerStyle}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
