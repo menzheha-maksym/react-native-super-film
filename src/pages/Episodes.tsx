f865c7d562a110ac9c0d585728c5b822379afe79
Maksym Menzheha <masimfyd@gmail.com>
2022-05-13 20:04:41 +0300

    show more, show less episodes

--- src/pages/Episodes.tsx
+++ src/pages/Episodes.tsx
@@ -1,6 +1,13 @@
 import moment from 'moment';
 import React, {useEffect, useState} from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import EpisodeComponent from '../components/EpisodeComponent';
 import {useAppDispatch, useAppSelector} from '../redux/hooks';
 import {selectDateString} from '../redux/reducers/calendarSlice';
@@ -10,6 +17,9 @@ import {
 } from '../redux/reducers/episodesSlice';
 
 const styles = StyleSheet.create({
+  container: {
+    height: Dimensions.get('window').height / 1.15,
+  },
   dateContainer: {
     paddingTop: 15,
     paddingBottom: 15,
@@ -20,37 +30,79 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontWeight: 'bold',
   },
+  showMore: {
+    width: '90%',
+
+    flexDirection: 'row',
+    justifyContent: 'center',
+
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'grey',
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 20,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  showMoreText: {
+    textAlign: 'center',
+  },
 });
 
 const Episodes: React.FC = ({}) => {
   const date = useAppSelector(selectDateString);
   const dispatch = useAppDispatch();
-  const [episodesData, setEpisodesData] = useState<Episode[]>();
+  const [allEpisodesData, setAllEpisodesData] = useState<Episode[]>();
+  const [shownEpisodesData, setShownEpisodesData] = useState<Episode[]>();
+  const [showingMore, setShowingMore] = useState(false);
 
   useEffect(() => {
     dispatch(fetchEpisodesByDateAsync(date))
       .unwrap()
       .then(episodes => {
-        setEpisodesData(episodes);
+        setAllEpisodesData(episodes);
+        setShownEpisodesData(episodes.slice(0, 4));
       });
   }, [date, dispatch]);
 
+  function handleShowMorePress() {
+    setShowingMore(true);
+    setShownEpisodesData(allEpisodesData);
+  }
+
+  function handleShowLessPress() {
+    setShowingMore(false);
+    setShownEpisodesData(allEpisodesData!.slice(0, 4));
+  }
+
   return (
-    <View>
+    <View style={styles.container}>
       <View style={styles.dateContainer}>
         <Text style={styles.dateText}>
           {moment(date).format('DD MMM YYYY')}
         </Text>
       </View>
-      <ScrollView>
-        {episodesData ? (
-          episodesData.map(episode => {
+
+      {shownEpisodesData ? (
+        <ScrollView>
+          {shownEpisodesData.map(episode => {
             return <EpisodeComponent key={episode.id} episode={episode} />;
-          })
-        ) : (
-          <Text>loading...</Text>
-        )}
-      </ScrollView>
+          })}
+
+          <TouchableOpacity
+            style={styles.showMore}
+            onPress={() =>
+              showingMore ? handleShowLessPress() : handleShowMorePress()
+            }>
+            <Text style={styles.showMoreText}>
+              {showingMore ? <Text>Show less</Text> : <Text>Show More</Text>}
+            </Text>
+          </TouchableOpacity>
+        </ScrollView>
+      ) : (
+        <Text>loading...</Text>
+      )}
     </View>
   );
 };
