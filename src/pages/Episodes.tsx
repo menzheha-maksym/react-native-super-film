96317e3c69d2aaf3d210fdc5938d78f779087bba
Maksym Menzheha <masimfyd@gmail.com>
2022-05-13 14:27:52 +0300

    fetch episodes on navigate to Episodes page

--- src/pages/Episodes.tsx
+++ src/pages/Episodes.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
+import {useAppDispatch, useAppSelector} from '../redux/hooks';
+import {selectDateString} from '../redux/reducers/calendarSlice';
+import {fetchEpisodesByDateAsync} from '../redux/reducers/episodesSlice';
 
 const Episodes: React.FC = ({}) => {
+  const date = useAppSelector(selectDateString);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchEpisodesByDateAsync(date))
+      .unwrap()
+      .then(episodes => {
+        console.log(episodes);
+      });
+  }, [date, dispatch]);
+
   return (
     <View>
       <Text>Episodes</Text>
